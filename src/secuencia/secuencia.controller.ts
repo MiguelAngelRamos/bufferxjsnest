ea9e3cd7406b12df30aa049c1d6669d56a1285ce
Miguel Angel Ramos <return.infox@gmail.com>
2024-06-17 18:41:54 -0400

    app nest buffersize

--- src/secuencia/secuencia.controller.ts
+++ src/secuencia/secuencia.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { SecuenciaService } from './secuencia.service';
+import { toArray } from 'rxjs';
 
-@Controller('secuencia')
-export class SecuenciaController {}
+@Controller('post')
+export class SecuenciaController {
+  constructor(private readonly secuenciaService: SecuenciaService) {}
+
+  @Get()
+  getPosts() {
+    return this.secuenciaService.getPosts();
+  }
+
+  @Get('buffered')
+  getBufferedPosts(@Query('size') bufferSize: number) {
+    return this.secuenciaService.getBufferedPosts(bufferSize).pipe(
+      toArray() // recoletar todos los buffer emitodos en un array
+    );
+  }
+}
--- src/secuencia/secuencia.service.ts
+++ src/secuencia/secuencia.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { bufferCount, from, map, mergeMap } from 'rxjs';
+import axios from 'axios';
 
 @Injectable()
 export class SecuenciaService {
 
   private readonly apiUrl = `https://jsonplaceholder.typicode.com/posts`;
 
-  getPosts() {
+  //* Metodo para obtener todos los posts
 
+  getPosts() {
+    return from(axios.get(this.apiUrl)).pipe(
+      map(response => response.data)
+    );
   }
 
-  getBufferedPost() {
-
+ //* Metodo para agrupar los posts en buffers (tamaño específico)
+  getBufferedPosts(bufferSize: number) {
+    return this.getPosts().pipe(
+      mergeMap(posts => from(posts).pipe(
+        // 100/5 = 20 buffersCount
+        bufferCount(bufferSize)
+      ))
+    )
   }
 
   getWindowedPosts() {
-    
+
   }
 }
